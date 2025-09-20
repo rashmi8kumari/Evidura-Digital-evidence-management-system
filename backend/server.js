7337bfa51396d80223e219138def828b91598bc1
Rashmi <rashmi8shahi@gmail.com>
2025-09-21 00:00:30 +0530

    Add backend build for render deployment

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use("/api/contact", contactRoutes);
 app.use(express.static(path.join(__dirname, "build")));
 
 // Catch-all route for React Router
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
