8deb527d4ba6abb68c87793bf74d8e56dbd927b9
Rashmi <rashmi8shahi@gmail.com>
2025-09-21 13:43:20 +0530

    removed old build

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,7 @@
 // src/utils/auth.js
 import axios from "axios";
 
-export const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000/api";
+export const API_BASE = process.env.REACT_APP_API_BASE || "/api";
 
 export const setToken = (token) => localStorage.setItem("token", token);
 export const getToken = () => localStorage.getItem("token");
